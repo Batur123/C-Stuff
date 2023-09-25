b2b3eddb55bc70f7cdb8e4a5f62208c045a4ced6
Batuhan Özkoç <batur.ozkoc@gmail.com>
2023-09-25 14:05:02 +0300

    Add other ops

--- src/math/sum/sum.cpp
+++ src/math/sum/sum.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
-#include "sum_class.h"
+#include "sum.h"
 
 int Sum(int X, int Y) {
     return X + Y;
 }
 
+int Multiply(int X, int Y) {
+    return X * Y;
+}
+
+int Divide(int X, int Y) {
+    return X / Y;
+}
+
+int Substract(int X, int Y) {
+    return X - Y;
+}
+
+void Add(int *X, int Y) {
+    *X = *X + Y;
+}
+
 int main() {
     std::cout << Sum(5, 4) << std::endl;
+    std::cout << Multiply(5, 4) << std::endl;
+    std::cout << Divide(5, 4) << std::endl;
+    std::cout << Substract(5, 4) << std::endl;
+
+    int Number = 5;
+    Add(&Number, 5);
+    std::cout << Number;
     return 0;
 }
---
+++ src/math/sum/sum.h
@@ -0,0 +1,9 @@
+#ifndef C_STUFF_SUM_H
+#define C_STUFF_SUM_H
+
+int Sum(int X, int Y);
+int Multiply(int X, int Y);
+int Divide(int X, int Y);
+int Substract(int X, int Y);
+
+#endif //C_STUFF_SUM_H
